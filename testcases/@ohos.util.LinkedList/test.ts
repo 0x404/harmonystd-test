3cd17be96f80484b4edc7c076b25c00e7a311a97
LilBeast922 <wsx79513@126.com>
2023-12-22 21:52:53 +0800

    linklist&plainarray

---
+++ testcases/@ohos.util.LinkedList/test.ts
@@ -0,0 +1,132 @@
+import LinkedList from '@ohos.util.LinkedList';
+
+function test_add(loopCount: number) {
+  // test: @ohos.util.linkedlist.add
+  // desc: 测试LinkedList容器add方法
+  const test_id = 1;
+
+  // prepare: setup objects to test
+  let linkedList = new LinkedList<any>();
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.add(`Element${i}`); // 在链表尾部插入元素
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_addFirst(loopCount: number) {
+  // test: @ohos.util.linkedlist.addFirst
+  // desc: 测试LinkedList容器addFirst方法
+  const test_id = 2;
+
+  // prepare: setup objects to test
+  const linkedList = new LinkedList<any>();
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.addFirst(`Element${i}`); // 在链表头部插入元素
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_insert(loopCount: number) {
+  // test: @ohos.util.linkedlist.insert
+  // desc: 测试LinkedList容器insert方法
+  const test_id = 3;
+
+  // prepare: 插入一些元素作为测试基础数据
+  const linkedList = new LinkedList<any>();
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.add(`Element${i}`); // 向链表尾部插入元素
+  }
+
+  // test
+  const indexToInsert = loopCount / 2; // 在链表中间位置插入元素
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.insert(indexToInsert, `NewElement${i}`); // 在指定索引位置插入元素
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_has(loopCount: number) {
+  // test: @ohos.util.linkedlist.has
+  // desc: 测试LinkedList容器has方法
+
+  const test_id = 4;
+  const linkedList = new LinkedList<any>();
+
+  // prepare
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.add(`Element${i}`); // 向链表尾部插入元素
+  }
+
+  // test
+  const elementToFind = `Element${loopCount / 2}`; // 在链表中间位置插入的元素
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.has(elementToFind); // 判断链表是否含有指定元素
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_get(loopCount: number) {
+  // test: @ohos.util.linkedlist.get
+  // desc: 测试LinkedList容器get方法
+
+  const test_id = 5;
+  const linkedList = new LinkedList<any>();
+
+  // prepare
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.add(`Element${i}`); // 向链表尾部插入元素
+  }
+
+  // test
+  const indexToGet = loopCount / 2; // 获取链表中间位置的元素
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    linkedList.get(indexToGet); // 根据索引获取链表中的元素
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+// 运行测试函数
+test_add(500000);
+test_addFirst(500000);
+test_insert(500000);
+test_has(500000);
+test_get(500000);
---
+++ testcases/@ohos.util.PlainArray/test.ts
@@ -0,0 +1,187 @@
+import PlainArray from '@ohos.util.PlainArray';
+
+function test_add(loopCount: number) {
+  // test: @ohos.util.PlainArray.add
+  // desc: 测试PlainArray容器add接口
+  const test_id = 1;
+
+  // prepare: setup objects to test
+  let plainArray = new PlainArray<string>();
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, "test");
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+function test_has(loopCount: number) {
+  // test: @ohos.util.PlainArray.has
+  // desc: 测试在最坏情况下（查找元素不存在的情况）PlainArray容器的has接口，List长度为500000
+  const test_id = 2;
+
+  // prepare: setup objects to test
+  let plainArray = new PlainArray<string>();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, "test");
+  }
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.has(500000);
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+function test_has2(loopCount: number) {
+  // test: @ohos.util.PlainArray.has
+  // desc: 测试在最优情况下（查找元素为第一个）PlainArray容器的has接口，List长度为5000000
+  const test_id = 3;
+
+  // prepare: setup objects to test
+  let plainArray = new PlainArray<string>();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, "test");
+  }
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.has(0);
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_get(loopCount: number) {
+  // test: @ohos.util.PlainArray.get
+  // desc: 测试PlainArray容器get方法
+
+  const test_id = 4;
+  const plainArray = new PlainArray<string>();
+
+  // prepare: setup objects to test
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, `Value${i}`);
+  }
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.get(i);
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_getIndexOfKey(loopCount: number) {
+  // test: @ohos.util.PlainArray.getIndexOfKey
+  // desc: 测试PlainArray容器getIndexOfKey方法（最坏情况，查找不存在的键）
+
+  const test_id = 5;
+  const plainArray = new PlainArray<string>();
+
+  // prepare: setup objects to test
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, `Value${i}`);
+  }
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.getIndexOfKey(500000);
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_getKeyAt(loopCount: number) {
+  // test: @ohos.util.PlainArray.getKeyAt
+  // desc: 测试PlainArray容器getKeyAt方法
+
+  const test_id = 6;
+  const plainArray = new PlainArray<string>();
+
+  // prepare: setup objects to test
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, `Value${i}`);
+  }
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.getKeyAt(i);
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+function test_clone(loopCount: number) {
+  // test: @ohos.util.PlainArray.clone
+  // desc: 测试PlainArray容器clone方法
+
+  const test_id = 7;
+  const plainArray = new PlainArray<string>();
+
+  // prepare: setup objects to test
+  for (let i = 0; i < loopCount; i++) {
+    plainArray.add(i, `Value${i}`);
+  }
+
+  // test
+  const startTime = new Date();
+  for (let i = 0; i < loopCount; i++) {
+    const clonedArray = plainArray.clone(); // 在这里进行 clone 操作，克隆一个新的实例
+  }
+  const endTime = new Date();
+
+  // finish
+  const executionTime = endTime.getTime() - startTime.getTime();
+  const averageTime = executionTime / loopCount;
+  console.log(`!${test_id}: ${averageTime} ms`);
+}
+
+
+
+
+
+// 运行测试函数
+test_add(500000);
+test_has(500000);
+test_has2(500000);
+test_get(500000);
+test_getIndexOfKey(500000);
+test_getKeyAt(500000);
+test_clone(500000);
