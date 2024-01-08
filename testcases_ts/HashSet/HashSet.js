5e7aaab1aa738808f383f71156f67a363b5d2d3c
0x404 <871206929@qq.com>
2024-01-08 14:23:48 +0800

    refactor directory

--- testcases_ts/HashSet/HashSet.js
+++ testcases_ts/HashSet/HashSet.js
@@ -1,8 +1,7 @@
 "use strict";
-// Test function using native JavaScript Set
 function test_set_add(loopCount) {
-    // test: Set.add
-    // desc: Test Set container add interface
+    // test: Set.prototype.add
+    // desc: 测试Set容器add接口
     const test_id = 1;
     // prepare: setup objects to test
     let set = new Set();
@@ -18,8 +17,8 @@ function test_set_add(loopCount) {
     console.log(`!${test_id}: ${averageTime} ms`);
 }
 function test_set_remove(loopCount) {
-    // test: Set.delete
-    // desc: Test Set container delete interface
+    // test: Set.prototype.delete
+    // desc: 测试Set容器delete接口
     const test_id = 2;
     // prepare: setup objects to test
     let set = new Set();
@@ -37,9 +36,9 @@ function test_set_remove(loopCount) {
     const averageTime = executionTime / loopCount;
     console.log(`!${test_id}: ${averageTime} ms`);
 }
-function test_set_has(loopCount) {
-    // test: Set.has
-    // desc: Test Set container has interface in the worst-case scenario (finding an element that doesn't exist), Set length is 500000
+function test_set_has_worst(loopCount) {
+    // test: Set.prototype.has
+    // desc: 测试在最坏情况下（查找元素不存在的情况）Set容器的has接口，set长度为500000
     const test_id = 3;
     // prepare: setup objects to test
     let set = new Set();
@@ -57,9 +56,9 @@ function test_set_has(loopCount) {
     const averageTime = executionTime / loopCount;
     console.log(`!${test_id}: ${averageTime} ms`);
 }
-function test_set_has2(loopCount) {
-    // test: Set.has
-    // desc: Test Set container has interface in the best-case scenario (finding the first element), Set length is 5000000
+function test_set_has_best(loopCount) {
+    // test: Set.prototype.has
+    // desc: 测试在最优情况下（查找元素为第一个）Set容器的has接口，set长度为5000000
     const test_id = 4;
     // prepare: setup objects to test
     let set = new Set();
@@ -78,8 +77,8 @@ function test_set_has2(loopCount) {
     console.log(`!${test_id}: ${averageTime} ms`);
 }
 function test_set_values(loopCount) {
-    // test: Set.values
-    // desc: Test Set container values interface, Set length is the input loopCount
+    // test: Set.prototype.values
+    // desc: 测试Set容器的values接口，set长度为输入的loopCount
     const test_id = 5;
     // prepare: setup objects to test
     let set = new Set();
@@ -98,8 +97,8 @@ function test_set_values(loopCount) {
     console.log(`!${test_id}: ${averageTime} ms`);
 }
 function test_set_entries(loopCount) {
-    // test: Set.entries
-    // desc: Test Set container entries interface, Set length is the input loopCount
+    // test: Set.prototype.entries
+    // desc: 测试Set容器的entries接口，set长度为输入的loopCount
     const test_id = 6;
     // prepare: setup objects to test
     let set = new Set();
@@ -118,8 +117,8 @@ function test_set_entries(loopCount) {
     console.log(`!${test_id}: ${averageTime} ms`);
 }
 function test_set_forEach(loopCount) {
-    // test: Set.forEach
-    // desc: Test Set container forEach interface, Set length is 500000
+    // test: Set.prototype.forEach
+    // desc: 测试Set容器的forEach接口,set长度为500000
     const test_id = 7;
     // prepare: setup objects to test
     let set = new Set();
@@ -130,6 +129,7 @@ function test_set_forEach(loopCount) {
     const startTime = new Date();
     for (let i = 0; i < loopCount; i++) {
         set.forEach((value, key) => {
+            // 模拟处理
         });
     }
     const endTime = new Date();
@@ -138,21 +138,11 @@ function test_set_forEach(loopCount) {
     const averageTime = executionTime / loopCount;
     console.log(`!${test_id}: ${averageTime} ms`);
 }
-//!end
+// 测试函数调用
 test_set_add(500000);
 test_set_remove(500000);
-test_set_has(500000);
-test_set_has2(500000);
+test_set_has_worst(500000);
+test_set_has_best(500000);
 test_set_values(500000);
 test_set_entries(500000);
 test_set_forEach(300);
-// Result
-/*
-!1: 0.000288 ms
-!2: 0.00025 ms
-!3: 0.000172 ms
-!4: 0.000144 ms
-!5: 0.000842 ms
-!6: 0.00074 ms
-!7: 106.626667 ms
-*/
